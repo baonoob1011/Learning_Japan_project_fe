35e4cc47da1861737d1996debcd46714068c112d
baonoob1011 <trandinhbao222@gmail.com>
2026-03-22 17:24:03 +0700

    Update next.config.ts

--- learning-app-fe/next.config.ts
+++ learning-app-fe/next.config.ts
@@ -3,10 +3,6 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  turbopack: {
-    // Chỉ định đúng thư mục root của project
-    root: path.resolve(__dirname),
-  },
 };
 
 export default nextConfig;
